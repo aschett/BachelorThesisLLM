ab3e2efab2cd1522683a04867cf947474faa3594
aschett <a.schett@outlook.de>
2024-11-11 13:25:55 +0100

    Add name guard

--- data_preperation_scripts/merge_augmented_datasets.py
+++ data_preperation_scripts/merge_augmented_datasets.py
@@ -1,32 +1,35 @@
 import pandas as pd
 
-file_paths = {
-    '../generated_datasets/memorable_concreteness.csv': 1,
-    '../generated_datasets/memorable_emotional.csv': 1,
-    '../generated_datasets/memorable_imagery.csv': 1,
-    '../generated_datasets/memorable_valence.csv': 1,
-    '../generated_datasets/non_memorable_texts.csv': 0
-}
+def merge_datasets():
+    file_paths = {
+        '../generated_datasets/memorable_concreteness.csv': 1,
+        '../generated_datasets/memorable_emotional.csv': 1,
+        '../generated_datasets/memorable_imagery.csv': 1,
+        '../generated_datasets/memorable_valence.csv': 1,
+        '../generated_datasets/non_memorable_texts.csv': 0
+    }
 
-dfs = []
+    dfs = []
 
-for file, memorable_value in file_paths.items():
+    for file, memorable_value in file_paths.items():
+        try:
+            df = pd.read_csv(file)
+
+            df['Memorable'] = memorable_value
+            dfs.append(df)
+
+        except FileNotFoundError:
+            print(f"Error: File {file} not found.")
+        except Exception as e:
+            print(f"An unexpected error occurred while processing {file}: {e}")
+
+    merged_df = pd.concat(dfs, ignore_index=True)
+
     try:
-        df = pd.read_csv(file)
-
-        df['Memorable'] = memorable_value
-        dfs.append(df)
-    
-    except FileNotFoundError:
-        print(f"Error: File {file} not found.")
+        merged_df.to_csv('../generated_datasets/merged_dataset.csv', index=False)
+        print("Dataset saved.")
     except Exception as e:
-        print(f"An unexpected error occurred while processing {file}: {e}")
+        print(f"Error saving: {e}")
 
-
-merged_df = pd.concat(dfs, ignore_index=True)
-
-try:
-    merged_df.to_csv('../generated_datasets/merged_dataset.csv', index=False)
-    print("Dataset saved.")
-except Exception as e:
-    print(f"Error saving: {e}")
+if __name__ == "__main__":
+    merge_datasets()
--- data_preperation_scripts/split_dataset.py
+++ data_preperation_scripts/split_dataset.py
@@ -1,18 +1,22 @@
-# Basic script to split train-test-validation data
+# Basic script to split the dataset
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-dataset_filepath = '../dataset/quotes_classification_data.csv'
-df = pd.read_csv(dataset_filepath)
+def split_dataset():
+    dataset_filepath = '../dataset/quotes_classification_data.csv'
+    df = pd.read_csv(dataset_filepath)
 
-#Split into  train (70%) and val and test each (15%)
-train_df, temp_df = train_test_split(df, test_size=0.3, random_state=42)
-val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
+    # Split into train (70%) and val and test each (15%)
+    train_df, temp_df = train_test_split(df, test_size=0.3, random_state=42)
+    val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
 
-#save the splits
-train_df.to_csv('../dataset_splits/train_dataset.csv', index=False)
-val_df.to_csv('../dataset_splits/val_dataset.csv', index=False)
-test_df.to_csv('../dataset_splits/test_dataset.csv', index=False)
+    # Save the splits
+    train_df.to_csv('../dataset_splits/train_dataset.csv', index=False)
+    val_df.to_csv('../dataset_splits/val_dataset.csv', index=False)
+    test_df.to_csv('../dataset_splits/test_dataset.csv', index=False)
 
-print("Datasets saved: train_dataset.csv, val_dataset.csv, test_dataset.csv")
+    print("Datasets saved: train_dataset.csv, val_dataset.csv, test_dataset.csv")
+
+if __name__ == "__main__":
+    split_dataset()
